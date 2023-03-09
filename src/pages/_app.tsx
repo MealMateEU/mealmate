7ac27d0c29f9fbc84d08c419200eb46d5b3cf6c3
Dmitrii <kopenkin.da@gmail.com>
2023-03-09 16:41:19 +0100

    Wrap App with DaisyUi Theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,20 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import { Theme } from "react-daisyui";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Theme dataTheme='valentine'>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Theme>
   );
 };
 
