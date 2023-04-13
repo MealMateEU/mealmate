d60565691d93af80655fdb7427a3a31767a3152c
Mickael GUY <99498833+GuyMickael@users.noreply.github.com>
2023-04-13 14:33:35 +0200

    Authentication & form (#5)
    * form done
    
    * pushing for help
    
    * updating user infos
    
    * redirections working
    
    * adding dependencies
    
    ---------
    
    Co-authored-by: Kopenkin Dmitrii <kopenkin.da@gmail.com>

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,6 +1,10 @@
 import { type NextPage } from "next";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Button } from "~/components/common/Button";
+import UserDataForm from "~/components/UserDataForm/UserDataForm";
+import { api } from "~/utils/api";
 
 function LoginForm() {
   return (
@@ -13,7 +17,33 @@ function LoginForm() {
 }
 
 const AuthPage: NextPage = () => {
-  return <LoginForm />;
+  const userLoggedIn = useSession().status === "authenticated";
+  const router = useRouter();
+  const [isNewUser, setIsNewUser] = useState(false);
+  const { data } = api.user.getUserInfos.useQuery();
+
+  // Here we check if the user is new or not
+  useEffect(() => {
+    if (data) {
+      if (
+        data.age === 0 &&
+        data.heightInCentimeters === 0 &&
+        data.objective === "UNSET"
+      ) {
+        setIsNewUser(true);
+        console.log(data.name, "-> Is a new user");
+      } else {
+        void router.push("/");
+      }
+    }
+  }, [data, router]);
+
+  // return part
+  if (!userLoggedIn) return <LoginForm />;
+
+  if (isNewUser) return <UserDataForm />;
+
+  return <></>;
 };
 
 export default AuthPage;
