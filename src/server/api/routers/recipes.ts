85b958c4483cccd80df84a8ae9fa38a293e63ea9
Dmitrii <kopenkin.da@gmail.com>
2023-04-24 22:47:09 +0200

    fix: integrated blacklist to recipes api call

--- src/server/api/routers/recipes.ts
+++ src/server/api/routers/recipes.ts
@@ -3,9 +3,12 @@ import { getExternalRecipes } from "../external/recipes.external";
 
 export const recipesRouter = createTRPCRouter({
   getSuggested: protectedProcedure.query(async ({ ctx }) => {
+    const blackListed = await ctx.prisma.blackList.findMany({
+      where: { userId: ctx.session.user.id },
+    });
     return await getExternalRecipes({
       type: "public",
-      excluded: ["chicken"],
+      excluded: blackListed.map((b) => b.food),
       calories: "600+",
     });
   }),
