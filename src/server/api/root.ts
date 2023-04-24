41b4affd7dd68e00888473b5872b9da7f30a9e77
Mickael GUY <99498833+GuyMickael@users.noreply.github.com>
2023-04-24 22:36:59 +0200

    select working with database like a bowss (#8)
    * select working with databasre
    
    * correcting package lock push
    
    * potential fix
    
    ---------
    
    Co-authored-by: Dmitrii <kopenkin.da@gmail.com>

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { userRouter } from "./routers/user";
 import { recipesRouter } from "./routers/recipes";
+import { blackListRouter } from "./routers/blackList";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { recipesRouter } from "./routers/recipes";
 export const appRouter = createTRPCRouter({
   user: userRouter,
   recipes: recipesRouter,
+  blacklist: blackListRouter
 });
 
 // export type definition of API
